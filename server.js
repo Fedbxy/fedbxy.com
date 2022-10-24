8656463ae05bc6b96ecc3dcfcf446a682f882689
Fed <punnaphop.sitthi@gmail.com>
2022-10-24 21:26:16 +0700

    ✨ added redirects

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'public'),{extensions: 'html'}));
 
 // Send the default page
-app.get('/', function(req, res) {
+app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname, 'public/home.html'));
 });
 
+app.get('/GitHub',function(req,res){
+    res.redirect('https://github.com/Fedbxy');
+});
+
+app.get('/114105099107114111108108',function(req,res){
+    res.redirect('https://www.youtube.com/watch?v=-AXetJvTfU0');
+});
+
 // Send the 404 page on code 404
-app.use(function(req, res, next) {
+app.use(function(req,res,next) {
     res.status(404).sendFile(path.join(__dirname, 'public/404.html'));
 });
 
